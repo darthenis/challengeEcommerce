a6822e528a0d428863890d0c41df7a899fff4fe4
Emiliano Acevedo <81443527+darthenis@users.noreply.github.com>
2023-03-22 19:34:35 -0300

    first commit

---
+++ src/main/java/com/easybuy/easybuy/EasybuyApplication.java
@@ -0,0 +1,13 @@
+package com.easybuy.easybuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasybuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easybuy/easybuy/EasybuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybuy.easybuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
