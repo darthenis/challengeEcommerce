a181038cfb96edebc5bbc434dc6451c65e5205cf
Emiliano Acevedo <81443527+darthenis@users.noreply.github.com>
2023-03-23 15:20:36 -0300

    update structure

---
+++ src/main/resources/static/web/js/index.js
@@ -0,0 +1,15 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            word : "Hola mundo"
+        }
+    },
+    created(){
+
+    },
+    methods: {
+
+    }
+}).mount("#app")
