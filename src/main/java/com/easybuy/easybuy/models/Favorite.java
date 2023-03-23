90e904640c361000c5c11686e8706a0dd59f0c0f
Emiliano Acevedo <81443527+darthenis@users.noreply.github.com>
2023-03-23 17:23:09 -0300

    added favorite model

---
+++ src/main/java/com/easybuy/easybuy/models/Favorite.java
@@ -0,0 +1,52 @@
+package com.easybuy.easybuy.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+
+    private String name;
+
+    private String ImgUrl;
+
+    public Favorite(){}
+
+    public Favorite(String name, String imgUrl) {
+        this.name = name;
+        ImgUrl = imgUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return ImgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        ImgUrl = imgUrl;
+    }
+}
