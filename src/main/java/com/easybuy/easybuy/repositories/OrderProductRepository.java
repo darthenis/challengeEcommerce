41511f8a130998555a431be690402d70e5e51b88
Emiliano Acevedo <81443527+darthenis@users.noreply.github.com>
2023-03-24 12:24:39 -0300

    added favoriteService

--- src/main/java/com/easybuy/easybuy/repositories/OrderProductRepository.java
+++ src/main/java/com/easybuy/easybuy/repositories/OrderProductRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-<<<<<<< HEAD:src/main/java/com/easybuy/easybuy/repositories/OrderProduct.java
-public interface OrderProduct extends JpaRepository <OrderProduct, Long> {
-=======
 public interface OrderProductRepository extends JpaRepository <OrderProduct,Long> {
->>>>>>> OrderProductService:src/main/java/com/easybuy/easybuy/repositories/OrderProductRepository.java
+
 }
