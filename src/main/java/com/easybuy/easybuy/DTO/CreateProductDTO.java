7bc9f592bad242e9cf3f390faa1d07393e8975a0
Tu nombre <Tu correo>
2023-03-24 14:00:52 -0300

    PRODUCT CONTROLLER

---
+++ src/main/java/com/easybuy/easybuy/DTO/CreateProductDTO.java
@@ -0,0 +1,7 @@
+package com.easybuy.easybuy.DTO;
+
+public class CreateProductDTO {
+
+
+
+}
--- src/main/java/com/easybuy/easybuy/controllers/ProductController.java
+++ src/main/java/com/easybuy/easybuy/controllers/ProductController.java
@@ -3,9 +3,9 @@ package com.easybuy.easybuy.controllers;
 import com.easybuy.easybuy.DTO.ProductDTO;
 import com.easybuy.easybuy.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,4 +23,9 @@ public class ProductController {
         return productService.findAll().stream().map(ProductDTO::new).collect(Collectors.toList());
 
     }
+
+    @PostMapping("/current/product/new")
+    public ResponseEntity<Object> createProduct(Authentication authentication, @RequestBody )
+
+
 }
