c985872be719e19c61c5942e7c6c3ab23184de04
Emiliano Acevedo <81443527+darthenis@users.noreply.github.com>
2023-03-26 13:58:53 -0300

    added service and controller rates and test

--- src/main/java/com/easybuy/easybuy/controllers/RateController.java
+++ src/main/java/com/easybuy/easybuy/controllers/RateController.java
@@ -1,22 +1,47 @@
 package com.easybuy.easybuy.controllers;
 
+import com.easybuy.easybuy.models.Client;
+import com.easybuy.easybuy.models.StarsEnum;
+import com.easybuy.easybuy.services.ClientService;
+import com.easybuy.easybuy.services.RateService;
 import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api")
 public class RateController {
 
+    @Autowired
+    RateService rateService;
+
+    @Autowired
+    ClientService clientService;
 
     @PreAuthorize("hasRole('CLIENT')")
     @PostMapping("/products/{id}/rates")
-    public ResponseEntity<?> addRate(Authentication authentication, @RequestBody String commentary){
+    public ResponseEntity<?> addRate(Authentication authentication, @RequestParam String commentary, @RequestParam StarsEnum starsEnum, @PathVariable Long id) {
 
-        return new ResponseEntity<>("added rate succesfully", HttpStatus.CREATED);
+        Optional<Client> client = clientService.findByEmail(authentication.getName());
+
+        try {
+
+            rateService.addRate(id, commentary, starsEnum, client.get());
+
+            return new ResponseEntity<>("added rate succesfully", HttpStatus.CREATED);
+
+        } catch (Exception e) {
+
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        }
+
+
 
     }
 
