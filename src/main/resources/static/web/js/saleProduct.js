579c6c8a0196b6c0e7cd2f02afdad60b2e44907e
Tu nombre <Tu correo>
2023-03-29 13:34:15 -0300

    finish logic cart

--- src/main/resources/static/web/js/saleProduct.js
+++ src/main/resources/static/web/js/saleProduct.js
@@ -7,18 +7,18 @@ createApp({
             radioPayments: null,
             bag: JSON.parse(localStorage.getItem("bag")) || [],
             active: null,
-            totalCart: 0
+            totalCart: 0,
+            totalCartQuantity: 0
+
+
         }
     },
     created() {
+        this.loadData()
         let parameterUrl = location.search
         let parameters = new URLSearchParams(parameterUrl)
         this.id = parameters.get("id")
-        this.loadData()
-    },
 
-    beforeMount() {
-        this.priceTotalCart()
     },
 
 
@@ -28,25 +28,25 @@ createApp({
                 .then(response => {
                     this.dataProduct = response.data
                     console.log(this.dataProduct)
-                    this.params
                     this.selectImg = this.dataProduct.imgsUrls[0]
+                    this.priceTotalCart()
                 })
         },
 
         changeImg(img) {
             this.selectImg = img
         },
-
+        /*----------------PARAMS ID PRODUCTO-------------------*/
         params() {
 
-            axios.get("/api/accounts/" + this.id)
+            axios.get("/api/products/" + this.id)
                 .then(res => {
                     this.accountSelect = res.data
                     this.transactionFilter = this.accountSelect.transactions
 
                 })
         },
-
+        /*------------------FORMATEO A MONEDA TIPO DOLAR US--------------*/
         formatDollar(price) {
             let USDollar = new Intl.NumberFormat('en-US', {
                 style: 'currency',
@@ -58,10 +58,9 @@ createApp({
         resetPayments() {
             this.radioPayments = null
         },
-
+        /*---------------AGREGAR AL CARRITO Y CANTIDAD----------------*/
         saveBag(object) {
             if (this.bag.find(item => item.id == object.id)) {
-                console.log("hola")
                 this.bag = this.bag.map(item => {
                     if (item.id == object.id && object.stock > 0 && object.stock > item.quantity) {
                         return { ...item, quantity: (item.quantity + 1) }
@@ -75,37 +74,58 @@ createApp({
                 this.bag.push(product)
             }
             localStorage.setItem("bag", JSON.stringify(this.bag))
-
+            this.quantityTotalCart()
+            this.priceTotalCart()
         },
 
+        /* -------------QUITAR CANTIDAD DEL CARRITO -------------*/
         outProductBag(object) {
             if (this.bag.find(item => item.id == object.id)) {
-                this.bag = this.bag.map(item => {
-                    if (item.id == object.id && object.quantity > 0) {
-                        console.log("hola")
-                        return { ...item, quantity: (item.quantity - 1) }
-                    } else {
-                        this.bag.pop(item);
-                    }
-                    localStorage.setItem("bag", JSON.stringify(this.bag))
-                })
-            }
-        },
-
-        priceTotalCart() {
-            let total = 0
-            this.bag.foreach(object => {
-                if (object.quantity > 0) {
-                    total += object.price * object.quantity
-                } else {
-                    total += object.price
+                object.quantity--
+                if (object.quantity === 0) {
+                    let index = this.bag.indexOf(this.bag.find(prod => prod.id === object))
+                    this.bag.splice(index, 1)
                 }
-            })
-            this.TotalCart = total
+            }
+            localStorage.setItem("bag", JSON.stringify(this.bag))
+            this.quantityTotalCart()
+            this.priceTotalCart()
+        },
+
+        /*--------------QUITAR POR COMPLETO DEL CARRITO-------------*/
+
+        removeItem(object) {
+            if (this.bag.find(item => item.id == object.id)) {
+                let index = this.bag.indexOf(this.bag.find(prod => prod.id === object))
+                this.bag.splice(index, 1)
+            }
+            localStorage.setItem("bag", JSON.stringify(this.bag))
+            this.quantityTotalCart()
+            this.priceTotalCart()
         },
 
 
-        toggleCar() {
+        /*--------------CALCULO DE TOTAL CARRITO--------------*/
+        priceTotalCart() {
+            let totalCount = 0
+            this.bag.forEach(object => {
+                totalCount += object.price * object.quantity
+            })
+            this.totalCart = totalCount
+        },
+
+        /*-----------------CANTIDAD DE PRODUCTOS-----------------*/
+        quantityTotalCart() {
+            let totalCount = 0
+            this.bag.forEach(object => {
+                totalCount += object.quantity
+            })
+            this.totalCartQuantity = totalCount
+        },
+
+
+        /*--------------TOGGLE CART--------------*/
+        toggleCart() {
 
             if (this.active == null) {
                 this.active = true;
