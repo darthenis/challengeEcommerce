0631ca8ea80104843a883a6830a74d27994cd86f
Emiliano Acevedo <81443527+darthenis@users.noreply.github.com>
2023-04-02 16:16:38 -0300

    added admin logic

--- src/main/resources/static/web/js/settingUser.js
+++ src/main/resources/static/web/js/settingUser.js
@@ -2,7 +2,15 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            isLogged: false
+            isLogged: false,
+            name : "",
+            lastName : "",
+            email : "",
+            oldPassword : "",
+            newPassword: "",
+            avatarUrl : null,
+            avatar : null
+            
 
         }
     },
@@ -25,6 +33,91 @@ createApp({
 
                     this.isLogged = false
                 })
+
+        },
+        editProfilePic(event){
+
+            console.log(event.target.files)
+
+            this.avatar = event.target.files;
+
+            this.avatarUrl = URL.createObjectURL(event.target.files[0])
+
+        },
+        handleMessageAlert(message, seconds, isError){
+
+            this.messageAlert = {
+                message,
+                isError
+            }
+
+            setTimeout(() => this.messageAlert.message = "", seconds * 1000)
+
+        },
+        updateInfo(){
+
+            if(this.avatar){
+
+                let formData = new FormData();
+
+                formData.append("avatar", this.avatar[0])
+
+                axios.post("/api/clients/auth/avatar", formData, { headers: { "Content-Type": "multipart/form-data" }})
+                                    .then(res => {
+
+                                        console.log(res)
+
+                                    }).catch(err => console.log(err))
+
+            }
+
+            if(this.newPassword && this.oldPassword){
+
+                axios.patch("/api/clients/current/password", `oldPassword=${this.oldPassword}&newPassword=${this.newPassword}`)
+                            .then(res => {
+
+                                if(this.name || this.lastName || this.email || this.tel){
+
+                                    axios.patch("/api/clients/current", {
+                                                                            name : this.name,
+                                                                            lastName :this.lastName,
+                                                                            email : this.email,
+                                                                            tel : this.tel
+                                                                        })
+                                                .then(res => {
+
+
+                                                    this.handleMessageAlert("updated succesfully", 3, false)
+
+                                                })
+
+                                } else {
+
+                                    this.handleMessageAlert("updated succesfully", 3, false)
+
+                                }
+
+                            })
+
+            } else {
+
+
+                if(this.name || this.lastName || this.email || this.tel){
+
+                    axios.patch("/api/clients/current", {
+                                                            name : this.name,
+                                                            lastName :this.lastName,
+                                                            email : this.email,
+                                                            tel : this.tel
+                                                        })
+                                        .then(res => {
+
+                                            this.handleMessageAlert("updated succesfully", 3, false)
+                                        })
+
+                }
+
+            }
 
         },
 
